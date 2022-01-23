a526b2ed9d9a0a933b4c05118dadf03bf90ef776
squidtempura <55754978+squidtempura@users.noreply.github.com>
2022-01-23 09:19:00 +0000

    spike damage

--- Assets/script/HealthBar.cs
+++ Assets/script/HealthBar.cs
@@ -7,20 +7,19 @@ public class HealthBar : MonoBehaviour
 {
     public Text healthText;
     public static int HealthCurrent;
-    public int HealthMax;
+    public static int HealthMax;
     private Image healthbar;
 
     // Start is called before the first frame update
     void Start()
     {
         healthbar = GetComponent<Image>();
-        HealthCurrent = HealthMax;
     }
 
     // Update is called once per frame
     void Update()
     {
-        healthbar.fillAmount = (float) HealthCurrent / HealthMax;
+        healthbar.fillAmount = (float) HealthCurrent / (float) HealthMax;
         healthText.text = HealthCurrent.ToString() + "/" + HealthMax.ToString();
     }
 }
--- Assets/script/playerHealth.cs
+++ Assets/script/playerHealth.cs
@@ -8,7 +8,8 @@ public class playerHealth : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        HealthBar.HealthMax = health;
+        HealthBar.HealthCurrent = health;
     }
 
     // Update is called once per frame
@@ -20,9 +21,11 @@ public class playerHealth : MonoBehaviour
     public void DamagePlayer(int damage)
     {
         health -= damage;
+        HealthBar.HealthCurrent = health;
         if(health <= 0)
         {
-            Destroy(gameObject);
+            health = 0;
         }
+        HealthBar.HealthCurrent = health;
     }
 }
