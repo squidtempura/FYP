396c678ad122509d96fce036634d9ca5223b6aa8
squidtempura <55754978+squidtempura@users.noreply.github.com>
2022-04-02 21:59:32 +0100

    test and fix bug in bullet

---
+++ Assets/script/PickUp.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PickUp : MonoBehaviour
+{
+    public int heal_value;
+    private PlayerHealth playerHealth;
+    private void Start()
+    {
+        playerHealth = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerHealth>();
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+
+        if(other.CompareTag("Player") && other.GetType().ToString() == "UnityEngine.CapsuleCollider2D")
+        {
+            playerHealth.HealPlayer(heal_value);
+            Destroy(gameObject);
+        }
+    }
+}
